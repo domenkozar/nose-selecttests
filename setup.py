c450feb0d933e9af4b8f2224eef74e0c597aba1d
Domen Kožar <domen@dev.si>
2016-02-22 22:01:21 +0000

    Back to development: 0.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version = '0.5'
+version = '0.6.dev0'
 
 setup(name='nose-selecttests',
       version=version,
